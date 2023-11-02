b4257df665c9f95a15c13e1755a9f4a908e365fa
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-02 17:29:53 +0100

    feat: add UnoCSS and Vitest config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import UnoCSS from '@unocss/svelte-scoped/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+			injectReset: '@unocss/reset/tailwind.css'
+		}),
+		sveltekit()
+	],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: 'src/setupTests.ts' 
 	}
 });
+
