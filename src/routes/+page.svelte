85caf28da52df1b3b056561965149ea152ccbe63
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-02 17:29:19 +0100

    feat: update index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="flex items-center">
+	<h1 class="text-5xl py-4">Welcome to SvelteKit!</h1>
+	<span class="i-carbon-logo-svelte text-6xl bg-[#f73c00]" />
+</header>
+<h2 class="text-xl">(and my template!)</h2>
+<p>
+	Visit <a class="underline decoration-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+	read the documentation
+</p>
